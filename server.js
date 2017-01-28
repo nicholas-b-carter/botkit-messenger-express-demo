d5dc0cb99fa0505c0aeb4dfa77946f831cafbf91
ncarter <ncarter@madmobile.com>
2017-01-28 13:37:06 -0500

    moar

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser') // BodyParser pour POST
 var http = require('http').Server(app)      // préparer le serveur web
 var dotenv = require('dotenv')
 var path = require('path')
+var opn = require('opn')
+var localtunnel = require('localtunnel')
 
 // configuration ===========================================
 
@@ -27,6 +29,22 @@ require('./app/routes/routes')(app)
 
 // port for Heroku
 app.set('port', (process.env.PORT || 5000))
+
+// LOCALTUNNEL ===================================================
+if (process.env !== 'production') {
+  var tunnel = localtunnel(app.get('port'), { subdomain: 'syscoassist' }, function(err, tunnel) {
+    let log = err ? `tunnel failz....... ${err}` : `everyday im tunnelin...... ${tunnel.url}`;
+    console.log(log);
+    if (!err) {
+      opn(tunnel.url);
+    }
+});
+
+tunnel.on('close', function() {
+  // tunnels are closed
+  console.log(`tunnelin......finished..... ${tunnel.url}`);
+});
+}
 
 // START ===================================================
 http.listen(app.get('port'), function () {
